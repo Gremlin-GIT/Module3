2aef7b4d4486c28241ac21f01c77ca54a00837c6
Gremlin-GIT <jadoba74@gmail.com>
2024-07-08 23:22:33 +0300

    Add files via upload

---
+++ modul_3_1.py
@@ -0,0 +1,27 @@
+def make_counter():
+    count = 0
+    def count_calls(func):
+        def wrapper(*args, **kwargs):
+            nonlocal count
+            count += 1
+            return func(*args, **kwargs)
+        return wrapper
+
+    def get_count():
+        return count
+
+    return count_calls, get_count
+count_calls, get_count = make_counter()
+
+@count_calls
+def string_info(string):
+    return len(string), string.upper(), string.lower()
+
+@count_calls
+def is_contains(string, list_to_search):
+    return string.lower() in map(str.lower, list_to_search)
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(get_count())
